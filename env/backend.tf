52fc91fa1fc40dccc36d94dd3b4b9b63921a65e4
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-09-21 23:46:14 +0530

    Update backend.tf

--- env/backend.tf
+++ env/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         =  "vstest123"
+    bucket         =  "vstestdemo369"
     key            = "tf/terraform.tfstate"
     region         = "us-east-1"
   }
